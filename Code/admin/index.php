ee30ab7f63d80f9016ab44d7ba2cbaffac1f1b34
Ramya2021 <77051852+Ramya2021@users.noreply.github.com>
2021-03-27 09:34:21 +0530

    Upated

--- Code/admin/index.php
+++ Code/admin/index.php
@@ -26,6 +26,7 @@
             
                        
                 <!-- /.row -->
+<!--
                 
 <div class="row">
     <div class="col-lg-3 col-md-6">
@@ -162,7 +163,7 @@
         </div>
     </div>
 </div>
-                <!-- /.row -->
+                 /.row 
                 
           <?php 
             
@@ -230,14 +231,203 @@
       }
                 
     </script>
-          
-        <div id="columnchart_material" style="width: 'auto'; height: 500px;"></div>  
-            
-            
-        </div>         
-                 
-                  
+ 
+    <div id="columnchart_material" style="width: 'auto'; height: 0px;"></div>  
+    </div> 
+-->
+    <div class="container" style="width:2000px; height:2000px">            
+    <script type="text/javascript" src="js/loader.js"></script>
+    <script>
+      google.charts.load('49', {'packages': ['vegachart']}).then(drawChart);
+
+      function drawChart() {
+        const dataTable = new google.visualization.DataTable();
+        dataTable.addColumn({type: 'string', 'id': 'category'});
+        dataTable.addColumn({type: 'number', 'id': 'amount'});
+        dataTable.addRows([
+          ['A', 28],
+          ['B', 55],
+          ['C', 43],
+          ['D', 91],
+          ['E', 81],
+          ['F', 53],
+          ['G', 19],
+          ['H', 87],
+        ]);
+
+        const options = {
+          "vega": {
+            "$schema": "https://vega.github.io/schema/vega/v4.json",
+            "width": 500,
+            "height": 200,
+            "padding": 5,
+
+            'data': [{'name': 'table', 'source': 'datatable'}],
+
+            "signals": [
+              {
+                "name": "tooltip",
+                "value": {},
+                "on": [
+                  {"events": "rect:mouseover", "update": "datum"},
+                  {"events": "rect:mouseout",  "update": "{}"}
+                ]
+              }
+            ],
+
+            "scales": [
+              {
+                "name": "xscale",
+                "type": "band",
+                "domain": {"data": "table", "field": "category"},
+                "range": "width",
+                "padding": 0.05,
+                "round": true
+              },
+              {
+                "name": "yscale",
+                "domain": {"data": "table", "field": "amount"},
+                "nice": true,
+                "range": "height"
+              }
+            ],
+
+            "axes": [
+              { "orient": "bottom", "scale": "xscale" },
+              { "orient": "left", "scale": "yscale" }
+            ],
+
+            "marks": [
+              {
+                "type": "rect",
+                "from": {"data":"table"},
+                "encode": {
+                  "enter": {
+                    "x": {"scale": "xscale", "field": "category"},
+                    "width": {"scale": "xscale", "band": 1},
+                    "y": {"scale": "yscale", "field": "amount"},
+                    "y2": {"scale": "yscale", "value": 0}
+                  },
+                  "update": {
+                    "fill": {"value": "steelblue"}
+                  },
+                  "hover": {
+                    "fill": {"value": "red"}
+                  }
+                }
+              },
+              {
+                "type": "text",
+                "encode": {
+                  "enter": {
+                    "align": {"value": "center"},
+                    "baseline": {"value": "bottom"},
+                    "fill": {"value": "#333"}
+                  },
+                  "update": {
+                    "x": {"scale": "xscale", "signal": "tooltip.category", "band": 0.5},
+                    "y": {"scale": "yscale", "signal": "tooltip.amount", "offset": -2},
+                    "text": {"signal": "tooltip.amount"},
+                    "fillOpacity": [
+                      {"test": "datum === tooltip", "value": 0},
+                      {"value": 1}
+                    ]
+                  }
+                }
+              }
+            ]
+          }
+        };
+
+        const chart = new google.visualization.VegaChart(document.getElementById('chart-div'));
+        chart.draw(dataTable, options);
+      }
+    </script>
+        <div id="chart-div" style="width: 700px; height: 250px; position: absolute;  top: 250px;
+  left: 300px;"></div>
                 
+    <script type="text/javascript">
+      google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Category', 'sales percentage'],
+          ['Categroy1',     11],
+          ['Categroy2',      2],
+          ['Categroy3',  2],
+          ['Categroy4', 2],
+          ['Categroy5',    7]
+        ]);
+
+        var options = {
+          title: 'Sales Reprt',
+          is3D: true,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+        chart.draw(data, options);
+      }
+    </script>
+        <div id="piechart_3d" style="width: 800px; height: 500px;  position: absolute;
+  top: 150px;
+  left: 1000px;"></div>
+        
+
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses'],
+          ['2004',  1000,      400],
+          ['2005',  1170,      460],
+          ['2006',  660,       1120],
+          ['2007',  1030,      540]
+        ]);
+
+        var options = {
+          title: 'Company Performance',
+          curveType: 'function',
+          legend: { position: 'right' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+           <div id="curve_chart" style="width: 1250px; height: 500px; position: absolute;  top: 650px;
+  left: 275px;"></div>
+        
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['bar']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses', 'Profit'],
+          ['2014', 1000, 400, 200],
+          ['2015', 1170, 460, 250],
+          ['2016', 660, 1120, 300],
+          ['2017', 1030, 540, 350]
+        ]);
+
+        var options = {
+          chart: {
+            title: 'Company Performance',
+            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+          },
+          bars: 'horizontal' // Required for Material Bar Charts.
+        };
+
+        var chart = new google.charts.Bar(document.getElementById('barchart_material'));
+
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+      }
+    </script>
+    <div id="barchart_material" style="width: 1100px; height: 450px; position: absolute;  top: 1200px;
+  left: 400px;"></div> 
             </div>
             <!-- /.container-fluid -->
 
