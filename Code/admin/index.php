a6e6ae19169a529187b877c23355dd649f416a99
Ramya2021 <77051852+Ramya2021@users.noreply.github.com>
2021-04-01 09:46:06 +0530

    Update index.php

--- Code/admin/index.php
+++ Code/admin/index.php
@@ -16,7 +16,7 @@
                            
                             Dashboard
                           
-                            <small> <?php  echo $_REQUEST['user_email'] ?></small>
+                            <small> <?php  echo $_REQUEST['user_firstname'] ?></small>
                             
                             
                         </h1>
